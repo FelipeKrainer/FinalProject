b7598933f33cce3f5e89310fc40216bdec8c1eaa
SpiffyJohnson <135999968+SpiffyJohnson@users.noreply.github.com>
2023-11-29 09:02:17 -0600

    Update Monster.h
    Added gold variable and function prototypes.

--- Monster.h
+++ Monster.h
@@ -7,8 +7,10 @@ class Monster {
             // Used for debugging. 
         std::string GetName() const;
             // Returns a monster's name.
-        int GetHP() const;
+        int GetMaxHP() const;
             // Returns a monster's maximum health points.
+        int GetCurrentHP() const;
+            // Returns a monster's current health points.
         int GetATK() const;
             // Returns the value stored in a monster's attack stat.
         int GetDEF() const;
@@ -17,12 +19,20 @@ class Monster {
             // Returns the value stored in a monster's agility stat.
         int GetEXP() const;
             // Returns the amount of experience a monster drops.
+        int GetGold() const;
+            // Returns the amount of gold a monster drops.
         std::string GetMethodOfAttack() const;
             // Returns the word that describes how a monster hits the player.
         void SetName(std::string);
             // Accepts a string and uses it to set a monster's name.
-        void SetHP(std::string);
+        void SetMaxHP(std::string);
             // Accepts a string and uses it to set a monster's maximum health points. 
+            // Converts the string to an integer.
+        void SetCurrentHP(int);
+            // Accepts a string and uses it to set a monster's current health points. 
+            // Converts the string to an integer.
+        void LowerCurrentHP(int);
+            // Accepts a string and uses it to lower a monster's current health points. 
             // Converts the string to an integer.
         void SetATK(std::string);
             // Accepts a string and uses it to set a monster's attack. 
@@ -36,14 +46,19 @@ class Monster {
         void SetEXP(std::string);
             // Accepts a string and uses it to set a monster's experience. 
             // Converts the string to an integer.
+        void SetGold(std::string);
+            // Accepts a string and uses it to set a monster's gold. 
+            // Converts the string to an integer.
         void SetMethodOfAttack(std::string);
             // Accepts a string and uses it to set the word used to describe a monster's attack.
     private:
         std::string Name;
-        int HP;
+        int MaxHP;
+        int CurrentHP;
         int ATK;
         int DEF;
         int AG;
         int EXP;
+        int Gold;
         std::string MethodOfAttack;
 };
